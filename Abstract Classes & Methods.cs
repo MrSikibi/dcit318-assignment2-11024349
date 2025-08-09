918442f32672c6186244b5f2c656c9d65598cc1a
MrSikib <mr.sikibi@gmail.com>
2025-08-09 11:49:33 +0000

    Implement abstract Shape class with Circle and Rectangle area calculation

---
+++ Abstract Classes & Methods.cs
@@ -0,0 +1,53 @@
+using System;
+
+// Abstract class
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+// Circle class
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+// Rectangle class
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class AbstractClassDemo
+{
+    static void Main()
+    {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+
+        Console.WriteLine("Circle Area: " + circle.GetArea());
+        Console.WriteLine("Rectangle Area: " + rectangle.GetArea());
+    }
+}
